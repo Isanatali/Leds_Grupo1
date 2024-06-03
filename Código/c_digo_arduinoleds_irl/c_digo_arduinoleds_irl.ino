61865445441cfc4a102e8e83d6852635fbb5515b
Isabella <Isanatali07@gmail.com>
2024-06-03 13:46:59 -0300

    Terceiro commit
    ajuste de algumas coisas e código do Leandro.

--- Código/c_digo_arduinoleds_irl/c_digo_arduinoleds_irl.ino
+++ Código/c_digo_arduinoleds_irl/c_digo_arduinoleds_irl.ino
@@ -1,29 +1,31 @@
 
 #define led_verde 11
 #define led_am 12
+#define led_verde2 10
 
 void setup()
 {
   pinMode(led_verde, OUTPUT);
   pinMode(led_am, OUTPUT);
+  pinMode(led_verde2, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(led_verde, HIGH);
-  delay(400); // Wait for 600 millisecond(s)
+  delay(400); // Espera por 400 milisegundos
   //liga o verde
   
   digitalWrite(led_verde, LOW);
-  delay(600); // Wait for 400 millisecond(s)
+  delay(600); // Espera por 600 milisegundos
   //desliga o verde
   
   digitalWrite(led_verde, HIGH);
-  delay(400); // Wait for 600 millisecond(s)
+  delay(400); // Espera por 400 milisegundos
   //liga o verde
   
   digitalWrite(led_verde, LOW);
-  delay(600); // Wait for 400 millisecond(s)
+  delay(600); // Espera por 600 milisegundos
   //desliga o verde
 
 
@@ -35,12 +37,29 @@ void loop()
   // Apagando LED amarelo
   digitalWrite(led_am, LOW);
   delay(100); // Esperar 100ms
-
-  digitalWrite(led_am, HIGH); 
-  delay(100);   
   
+  // Ligando LED amarelo
+  digitalWrite(led_am, HIGH); 
+  delay(100); // Esperar 100ms
+    
+  // Apagando LED amarelo
   digitalWrite(led_am, LOW);
-  delay(100); 
+  delay(100); // Esperar 100ms
+
+ 
+ 
+  //Led verde 2
+  digitalWrite(13, HIGH);//liga led 
+  delay(200);//espera 200ms
+  
+  digitalWrite(13, LOW);//apaga led
+  delay(200);//espera 200ms
+  
+  digitalWrite(13, HIGH);//liga led
+  delay(200);//espera 200ms
+  
+  digitalWrite(13, LOW);//apaga o led
+  delay(1000);//espera 1000ms
 
 
 }
