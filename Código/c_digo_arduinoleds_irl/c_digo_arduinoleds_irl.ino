6e0424d8fa3617348374c0bbc331127f3b64fe24
Isabella <Isanatali07@gmail.com>
2024-06-03 14:03:15 -0300

    Sexto commit
    Organização e acréscimo de nomes.

--- Código/c_digo_arduinoleds_irl/c_digo_arduinoleds_irl.ino
+++ Código/c_digo_arduinoleds_irl/c_digo_arduinoleds_irl.ino
@@ -1,21 +1,22 @@
 
+#define led_verm2 9
+#define led_verde2 10
 #define led_verde 11
 #define led_am 12
-#define led_verde2 10
 #define led_verm 13
-#define led_verm2 9
 
 void setup()
 {
   pinMode(led_verde, OUTPUT);
+  pinMode(led_verm, OUTPUT);
   pinMode(led_am, OUTPUT);
   pinMode(led_verde2, OUTPUT);
-  pinMode(led_verm, OUTPUT);
   pinMode(led_verm2, OUTPUT);
 }
 
 void loop()
 {
+  //Led verde - Alexya
   digitalWrite(led_verde, HIGH);
   delay(400); // Espera por 400 milisegundos
   //liga o verde
@@ -34,7 +35,22 @@ void loop()
 
 
 
-  // Ligando LED amarelo
+  //Led vermelho - Isabella
+  digitalWrite(led_verm, HIGH); //Acende led vemelho
+  delay(2000); //Espera 2 segundos
+
+  digitalWrite(led_verm, LOW); //Apaga led vermelho
+  delay(2000); //Espera 2 segundos
+
+  digitalWrite(led_verm, HIGH); //Acende led vemelho
+  delay(2000); //Espera 2 segundos
+
+  digitalWrite(led_verm, LOW); //Apaga led vermelho
+  delay(2000); //Espera 2 segundos
+
+
+
+  // Ligando LED amarelo - Gabriel
   digitalWrite(led_am, HIGH); 
   delay(100); // Esperar 100ms  
   
@@ -50,9 +66,9 @@ void loop()
   digitalWrite(led_am, LOW);
   delay(100); // Esperar 100ms
 
+
  
- 
-  //Led verde 2
+  //Led verde 2 - Leandro
   digitalWrite(led_verde2, HIGH);//liga led 
   delay(200);//espera 200ms
   
@@ -67,23 +83,7 @@ void loop()
 
 
 
-
-  //Led vermelho
-  digitalWrite(led_verm, HIGH); //Acende led vemelho
-  delay(2000); //Espera 2 segundos
-
-  digitalWrite(led_verm, LOW); //Apaga led vermelho
-  delay(2000); //Espera 2 segundos
-
-  digitalWrite(led_verm, HIGH); //Acende led vemelho
-  delay(2000); //Espera 2 segundos
-
-  digitalWrite(led_verm, LOW); //Apaga led vermelho
-  delay(2000); //Espera 2 segundos
-
-
-
-  //Led vermelho 2
+  //Led vermelho 2 - Thalita
   digitalWrite(led_verm2, HIGH); //Acende o segundo led vemelho
   delay(1000); //Espera 1 segundos
 
